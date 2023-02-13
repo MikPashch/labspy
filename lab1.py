6f0260c98ae6c017d308cb117f99a334be9cce93
MikPashch <mishapashenko@gmail.com>
2023-02-13 16:37:16 +0100

    I dont khow how does it work, just deleted old directory & files recolacted on 1 step above

---
+++ lab1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = 1
+e = np.e
+sin = np.sin
+
+y = ((e ** (1 / (sin(x) + 1))) / ((5 / 4) + (1 / ((x ** 1) * 5))))
+log = np.log(y) / np.log(1 + x ** 2)
+
+print(log)
+
+x = np.arange(-10, 10.01, 0.01)
+plt.plot(x, x * x - x - 6)
+plt.grid(True)
+
+plt.show()
