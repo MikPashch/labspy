403ec8458d25b80740da4f546b56e90dfcdf6119
MikPashch <mishapashenko@gmail.com>
2023-02-13 16:28:02 +0100

    Add new files & start of lab2

--- lab1/lab1_ex_se5.py
+++ lab1/lab1_ex_se5.py
@@ -1,16 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5, 6]
-y = [1, 1.42, 1.76, 2, 2.24, 2.5]
-p, v = np.polyfit(x, y, deg=1, cov=True)
-
->>>p
-array([0.28517032, 0.80720757])
->>> v
-array([[0.00063242, -0.00221348],
-[-0.00221348, 0.00959173]])
-
-p_f = np.poly1d(p)
-p_f(0.5)
-p_f([1, 2, 3])
+x = np.arange(0, 10, 0.01)
+plt.plot(x, x**2, label=r'$f = x^2$')
+plt.scatter(x, x**2 + np.random.randn(len(x))*x, s=0.3)
+plt.fill_between(x, 1.3*x**2, 0.7*x**2, alpha=0.3)
+plt.legend(loc='best')
+plt.savefig('figure_fill_between.png')
+plt.show()
