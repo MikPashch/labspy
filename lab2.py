b77be0ebec4cf74c9302811bf5abc0611e6c2963
MikPashch <mishapashenko@gmail.com>
2023-02-15 13:37:22 +0100

    Turtle 10 squares

--- lab2.py
+++ lab2.py
@@ -1,7 +1,17 @@
 import turtle
 
 turtle.shape('turtle')
-a = 1
-for a in range(360):
-    turtle.forward(1)
-    turtle.left(1)
+x = 50
+step = 0
+for i in range(10):
+
+    for s in range(4):
+        turtle.forward(x)
+        turtle.left(90)
+
+    x += 40
+    step -= 20
+    turtle.penup()
+    turtle.goto(step, step)
+    turtle.pendown()
+
