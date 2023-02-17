ac97d96d21260b8f5b8a8686def8ee74332eae83
MikPashch <mishapashenko@gmail.com>
2023-02-17 17:41:28 +0100

    Turtle triangle & square & gexagons

--- lab2.py
+++ lab2.py
@@ -1,9 +1,24 @@
 import turtle
+import numpy as np
 
 turtle.shape('turtle')
-x = 50
+
+r = 50
+n = 3
+a = r * 2 * np.sin(np.pi / 180 * (360 / (2 * n)))
+alpha = (360 / (2 * n))     # 60degree
+delta = 25
+
 for i in range(15):
-    for j in range(2):
-        turtle.forward(x)
-        turtle.left(90)
-    x += 20
+    turtle.left(180 - (alpha / 2))
+    for y in range(n):
+        turtle.forward(a)
+        turtle.left(360 / n)
+    turtle.right(180 - (alpha / 2))
+    turtle.penup()
+    turtle.forward(delta)
+    turtle.pendown()
+    n += 1
+    r = r + delta
+    a = r * 2 * np.sin(np.pi / 180 * (360 / (2 * n)))
+    alpha = 180 - (360 / n)
