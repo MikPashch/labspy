7d3e8a2350e660b77ead94edfb6b9e640e0a9162
MikPashch <mishapashenko@gmail.com>
2023-02-20 10:48:58 +0100

    Turtle 1st flower

--- lab2.py
+++ lab2.py
@@ -3,22 +3,7 @@ import numpy as np
 
 turtle.shape('turtle')
 
-r = 50
-n = 3
-a = r * 2 * np.sin(np.pi / 180 * (360 / (2 * n)))
-alpha = (360 / (2 * n))     # 60degree
-delta = 25
-
-for i in range(15):
-    turtle.left(180 - (alpha / 2))
-    for y in range(n):
-        turtle.forward(a)
-        turtle.left(360 / n)
-    turtle.right(180 - (alpha / 2))
-    turtle.penup()
-    turtle.forward(delta)
-    turtle.pendown()
-    n += 1
-    r = r + delta
-    a = r * 2 * np.sin(np.pi / 180 * (360 / (2 * n)))
-    alpha = 180 - (360 / n)
+for i in range(3):
+    turtle.circle(50, 360)
+    turtle.circle(-50, 360)
+    turtle.left(360 / 6)
