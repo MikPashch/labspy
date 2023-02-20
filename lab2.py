d2620a9b86d7be8b8a4f66c8f796ffd3ba4e78f4
MikPashch <mishapashenko@gmail.com>
2023-02-20 13:22:22 +0100

    Turtle 1st star & the end of lab2

--- lab2.py
+++ lab2.py
@@ -1,43 +1,13 @@
+import turtle
 import turtle as t
 
 t.shape("turtle")
-t.penup()
-t.goto(0, - 200)
-t.pendown()
 
+a = 500
+turtle.left(180)
+t.forward(a)
+n = 11
 
-t.begin_fill()
-t.color("yellow")
-t.circle(200)
-t.end_fill()
-t.penup()
-
-t.goto(-90, 50)
-t.pendown()
-t.begin_fill()
-t.color('blue')
-t.circle(35)
-t.end_fill()
-t.penup()
-
-t.goto(90, 50)
-t.pendown()
-t.begin_fill()
-t.color('blue')
-t.circle(35)
-t.end_fill()
-t.penup()
-t.goto(0, 20)
-t.right(90)
-t.color('black')
-t.pendown()
-t.width(10)
-t.forward(40)
-t.penup()
-
-t.goto(100, -40)
-t.color('red')
-t.pendown()
-t.width(10)
-t.circle(-100, 180)
-t.penup()
+for i in range(n - 1):
+    t.left(180 - 360/(2*n))
+    t.forward(a)
