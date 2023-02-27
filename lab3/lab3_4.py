eb61e653798225f107e39b85a9a8bee70dfebbff
MikPashch <mishapashenko@gmail.com>
2023-02-27 11:36:57 +0100

    Half of success

--- lab3/lab3_4.py
+++ lab3/lab3_4.py
@@ -8,11 +8,14 @@ Vx = 20  # m/s
 Vy = 20  # m/s
 dt = 0.1  # s
 ay = - 9.8  # m/s**2
+el_cof = 0.7
 
 while Vy > 1:
-    while y > 0:
+    while y >= 0:
         turtle.goto(x, y)
         x += Vx * dt
         y += Vy * dt + ay * (dt ** 2) / 2
         Vy += ay * dt
-
+    y = 0
+    Vy = Vx * el_cof
+    Vx = Vx * el_cof
