c3a9d6fbab593a1ec1e59a8667704a4e79cf3441
MikPashch <mishapashenko@gmail.com>
2023-02-27 09:57:17 +0100

    1 fist cycle - succes

---
+++ lab3/lab3_4.py
@@ -0,0 +1,18 @@
+import turtle
+
+turtle.shape('turtle')
+
+x = 30  # m
+y = 30  # m
+Vx = 20  # m/s
+Vy = 20  # m/s
+dt = 0.1  # s
+ay = - 9.8  # m/s**2
+
+while Vy > 1:
+    while y > 0:
+        turtle.goto(x, y)
+        x += Vx * dt
+        y += Vy * dt + ay * (dt ** 2) / 2
+        Vy += ay * dt
+
