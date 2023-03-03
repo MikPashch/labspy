2e71b937e70b8b5021daeeb488d2c9d13d139118
MikPashch <mishapashenko@gmail.com>
2023-03-03 12:25:54 +0100

    Second version of smile(without angle of brows)

--- lab4/lab4_1.py
+++ lab4/lab4_1.py
@@ -1,25 +1,38 @@
 import pygame
 from pygame.draw import *
 
+# Initializing Pygame
 pygame.init()
 
 FPS = 30
-screen = pygame.display.set_mode((800, 400))
+
+# Initializing surface
+screen = pygame.display.set_mode((800, 350))
+
+# Initializing RGB Color for screen
+color = (210, 210, 210)
+
+# Changing surface color
+screen.fill(color)
+pygame.display.flip()
 
 # smile's face
 circle(screen, (255, 255, 50), (400, 175), 150)
 circle(screen, (0, 0, 0), (400, 175), 150, 2)
 
 # smile's eys
-circle(screen, (255, 0, 0), (460, 120), 30)     # left
-circle(screen, (0, 0, 0), (460, 120), 14)
+circle(screen, (255, 0, 0), (460, 130), 30)     # left
+circle(screen, (0, 0, 0), (460, 130), 14)
 
-circle(screen, (255, 0, 0), (340, 120), 35)     # right
-circle(screen, (0, 0, 0), (340, 120), 18)
+circle(screen, (255, 0, 0), (340, 130), 35)     # right
+circle(screen, (0, 0, 0), (340, 130), 18)
+
+# brows
+brow_left = rect(screen, (0, 0, 0), ((460-120/2, 130-30-15), (120, 15)))   # left
+brow_right = rect(screen, (0, 0, 0), ((340-120/2, 130-35-15), (120, 15)))   # right
 
 # mouse
-rect(screen, (0, 0, 0), (300, 160, 200, 150))
-
+rect(screen, (0, 0, 0), (325, 250, 150, 25))
 
 pygame.display.update()
 clock = pygame.time.Clock()
